7b5bd5386cb1bbec84a1d1ed781658724f5233ad
Liam Boyd <notliamb@outlook.com>
2024-02-29 23:26:58 +0000

    Added files

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,127 @@
-import 'package:restaurant_finder/example_candidate_model.dart';
-import 'package:restaurant_finder/example_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Example(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class Example extends StatefulWidget {
-  const Example({
-    super.key,
+class Restaurant {
+  final String name;
+  final String typeOfFood;
+  final String address;
+
+  Restaurant({
+    required this.name,
+    required this.typeOfFood,
+    required this.address,
   });
-
-  @override
-  State<Example> createState() => _ExamplePageState();
 }
 
-class _ExamplePageState extends State<Example> {
-  final CardSwiperController controller = CardSwiperController();
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Restaurant List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RestaurantListScreen(),
+    );
+  }
+}
 
-  final cards = candidates.map(ExampleCard.new).toList();
+class RestaurantListScreen extends StatefulWidget {
+  @override
+  _RestaurantListScreenState createState() => _RestaurantListScreenState();
+}
+
+class _RestaurantListScreenState extends State<RestaurantListScreen> {
+  late List<Restaurant> restaurants = [];
+  bool isLoading = false;
 
   @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    _fetchRestaurants();
+  }
+
+  Future<void> _fetchRestaurants() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      final double latitude = 53.2788691; // Set your latitude here
+      final double longitude = -74.0060; // Set your longitude here
+      final String apiKey =
+          's9667g9q9FuzSJzcLzzgZv11ChjMSrIS27KrY370HVVpUEOu79jiOrOrHp1JUNO-v99wl1TQ8g973nnpnjKKJ3XxKbNKNL2Q2d6125jGsncm75EbZhrR9Ws8olzwZXYx'; // Replace with your Yelp API key
+      final String url =
+          'https://api.yelp.com/v3/businesses/search?latitude=$latitude&longitude=$longitude&categories=restaurants&limit=50';
+
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {'Authorization': 'Bearer $apiKey'},
+      );
+
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body);
+        setState(() {
+          restaurants = List<Restaurant>.from(jsonData['businesses'].map((data) =>
+              Restaurant(
+                name: data['name'],
+                typeOfFood: data['categories'][0]['title'],
+                address: data['location']['address1'] ?? "",
+              )));
+        });
+      } else {
+        throw Exception('Failed to load restaurants');
+      }
+    } catch (e) {
+      print('Error: $e');
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  List<Container> resCards() {
+    List<Container> cards = [];
+    for (int i = 0; i < restaurants.length; i++) {
+      final restaurant = restaurants[i];
+      final color = i.isEven ? Colors.blue : Colors.green; // Alternate colors
+      cards.add(
+        Container(
+          alignment: Alignment.center,
+          child: Text(
+            restaurant.name, 
+            style: TextStyle(color: Colors.white),
+          ),
+          
+          color: color,
+          padding: EdgeInsets.all(16.0),
+          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        ),
+      );
+    }
+    return cards;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Flexible(
-              child: CardSwiper(
-                controller: controller,
-                cardsCount: cards.length,
-                onSwipe: _onSwipe,
-                onUndo: _onUndo,
-                numberOfCardsDisplayed: 3,
-                backCardOffset: const Offset(40, 40),
-                padding: const EdgeInsets.all(24.0),
-                cardBuilder: (
-                  context,
-                  index,
-                  horizontalThresholdPercentage,
-                  verticalThresholdPercentage,
-                ) =>
-                    cards[index],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  
-                  FloatingActionButton(
-                    onPressed: () => controller.swipe(CardSwiperDirection.left),
-                    child: const Icon(Icons.close),
-                    backgroundColor: Colors.red,
-                    
-                  ),
-                  FloatingActionButton(
-                    onPressed: () =>
-                        controller.swipe(CardSwiperDirection.right),
-                    child: const Icon(Icons.done),
-                    backgroundColor: Colors.green,
-                  ),
-                  
-                ],
-              ),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Restaurant List'),
       ),
+      body: Flexible(
+        child: CardSwiper(
+          cardsCount: resCards().length,
+          cardBuilder: (context, index, percentThresholdX, percentThresholdY) => resCards()[index],
+        )
+      ),
+          
     );
-  }
-
-  bool _onSwipe(
-    int previousIndex,
-    int? currentIndex,
-    CardSwiperDirection direction,
-  ) {
-    debugPrint(
-      'The card $previousIndex was swiped to the ${direction.name}. Now the card $currentIndex is on top',
-    );
-    return true;
-  }
-
-  bool _onUndo(
-    int? previousIndex,
-    int currentIndex,
-    CardSwiperDirection direction,
-  ) {
-    debugPrint(
-      'The card $currentIndex was undod from the ${direction.name}',
-    );
-    return true;
   }
 }
